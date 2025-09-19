f7ecf8b85bd1f945d9069285cb3e11f8180eb41e
爱吃元包的皮蛋 <94884381+yuanbaoerer@users.noreply.github.com>
2025-09-20 00:43:29 +0800

    Initial project

---
+++ src/main/java/org/example/nitroaicoder/NitroAiCoderApplication.java
@@ -0,0 +1,13 @@
+package org.example.nitroaicoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitroAiCoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitroAiCoderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nitroaicoder/NitroAiCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nitroaicoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitroAiCoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
