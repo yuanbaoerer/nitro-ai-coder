f1d3284ce1323095e7568137e66e9bf4b5ee7c77
爱吃元包的皮蛋 <94884381+yuanbaoerer@users.noreply.github.com>
2025-09-21 00:26:06 +0800

    后端初始框架搭建

--- src/main/java/org/example/nitroaicoder/common/BaseResponse.java
+++ src/main/java/org/example/nitroaicoder/common/BaseResponse.java
@@ -12,21 +12,24 @@ import java.io.Serializable;
  */
 @Data
 public class BaseResponse<T> implements Serializable {
+
     private int code;
+
     private T data;
+
     private String message;
 
-    public BaseResponse(int code, T data, String message){
+    public BaseResponse(int code, T data, String message) {
         this.code = code;
         this.data = data;
         this.message = message;
     }
 
-    public BaseResponse(int code, T data){
+    public BaseResponse(int code, T data) {
         this(code, data, "");
     }
 
-    public BaseResponse(ErrorCode errorCode){
+    public BaseResponse(ErrorCode errorCode) {
         this(errorCode.getCode(), null, errorCode.getMessage());
     }
 }
--- src/main/java/org/example/nitroaicoder/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/nitroaicoder/exception/GlobalExceptionHandler.java
@@ -16,15 +16,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public BaseResponse<?> businessExceptionHandler(BusinessException e)
-    {
-        log.error("businessExceptionHandler", e);
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
         return ResultUtils.error(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e){
-        log.error("runtimeExceptionHandler", e);
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
     }
 }
