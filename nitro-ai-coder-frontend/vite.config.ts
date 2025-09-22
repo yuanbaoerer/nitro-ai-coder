b4f572e4815dec65ebc1ac7c354c32f48468a7d4
爱吃元包的皮蛋 <94884381+yuanbaoerer@users.noreply.github.com>
2025-09-22 23:59:54 +0800

    前端 1、用户管理页面

--- nitro-ai-coder-frontend/vite.config.ts
+++ nitro-ai-coder-frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
